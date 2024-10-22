e8611c9fc9a022d0b310084d89eebdeb561afc1f
ayatullahkam <ayatullahahkam004@gmail.com>
2024-10-22 15:40:17 +0700

    git init user

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.auth-login');
 });
 
-Route::get('/dashboard', function () {
-    return view('pages.dashboard', ['type_menu' => 'dashboard']);
+// Route::get('/dashboard', function () {
+//     return view('pages.dashboard', ['type_menu' => 'dashboard']);
+
+// });
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('home', function () {
+        return view('pages.dashboard', ['type_menu' => 'home']);
+    })->name('home');
+
+    Route::resource('user',UserController::class);
+
 });
